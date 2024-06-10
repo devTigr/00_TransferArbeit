cc1d57acc4c5b8fcb66034d0f3445458f0822cad
devTigr <133799785+devTigr@users.noreply.github.com>
2024-06-10 17:27:35 +0200

    Consumer gestartet noch nicht lauffähig

---
+++ 03_Consumer_(Eigenentwicklung)/go.mod
@@ -0,0 +1,8 @@
+module main.go
+
+go 1.21.4
+
+require (
+	github.com/streadway/amqp v1.1.0
+	go.mongodb.org/mongo-driver v1.15.0
+)
---
+++ 03_Consumer_(Eigenentwicklung)/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// failOnError is a helper function to log error messages
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+type StockEvent struct {
+	Company   string  `json:"company"`
+	EventType string  `json:"eventType"`
+	Price     float64 `json:"price"`
+}
+
+func main() {
+	//Connect to DBCluster
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/"))
+	failOnError(err, "Failed to create a user for MongoDB")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	failOnError(err, "Failed to connect to MongoDB")
+
+	defer client.Disconnect(ctx)
+
+	//connect to RabbitMQ
+	conn, err := amqp.Dial("amqp://stockmarket:supersecret123@localhost:5672/")
+
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	queueName := os.Getenv("QUEUENAME")
+
+	if queueName == "" {
+		err := errors.New("queue missing")
+		failOnError(err, "Failed to fetch the QUEUENAME from Environment")
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName, // name of the queue
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var events []StockEvent
+	counter := 0
+
+	for message := range msgs {
+		var event StockEvent
+		err := json.Unmarshal(message.Body, &event)
+		if err != nil {
+			log.Printf("Failed to unmarshal message: %s", err)
+			continue
+		}
+
+		events = append(events, event)
+		counter++
+
+		if counter == 1000 {
+			// Aggregate the events here
+			// Reset the events slice and counter
+			events = []StockEvent{}
+			aggregatedPrice := 0
+
+			for event := range events {
+				aggregatedPrice ==  aggregatedPrice +)
+			}
+			// todo: push to DB
+
+		}
+	}
+}
